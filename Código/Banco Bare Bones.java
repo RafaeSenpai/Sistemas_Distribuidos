d926b18f14d5caf04451cccced48e85ffaff52e0
“Rafael” <rafael.alves.proff@gmail.com>
2019-09-06 00:30:49 +0100

    Adicionado parte do conteudo da disciplina

---
+++ Código/Banco Bare Bones.java
@@ -0,0 +1,62 @@
+import java.util.Random;
+
+class Mover implements Runnable 
+{
+  Bank b;
+
+  public Mover(Bank b) { this.b = b; }
+
+  public void run() 
+  {
+    Random rand = new Random();
+    int slots=b.slots();
+    int f;
+    int t, tries;
+    for(tries=0; tries<1000000; tries++)
+    { 
+      f=rand.nextInt(slots); // get one
+      while((t=rand.nextInt(slots))==f); // get a distinct other. Carefull!
+      b.take(f,10);
+      b.put(t,10);
+    }
+  }
+}
+
+class Adder implements Runnable 
+{
+  Bank b;
+
+  public Adder(Bank b) { this.b = b; }
+
+  public void run() 
+  {
+    int slots=b.slots();
+    int sum, i, tries, lsum=0;
+    for(tries=0; tries<1000000; tries++)
+    { 
+      sum=0;
+      for (i=0; i<slots; i++)
+      {
+        sum+=b.query(i);
+      }
+      if (lsum!=sum) System.out.println("Total "+sum);
+      lsum=sum;
+    }
+  }
+}
+
+
+
+class ExBankTransf3 
+{
+  public static void main(String[] args) 
+  {
+    int N = 10; // Number of accounts
+    Bank b = new Bank(N);
+    Mover m = new Mover(b);
+    Adder c = new Adder(b);
+
+    new Thread(m).start();
+    new Thread(c).start();
+  }
+}
